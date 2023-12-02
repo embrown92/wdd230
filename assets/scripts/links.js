fef4929d258ef72d4b541bdf2a0b336127dc341c
Eda Mae Brown <eda_awe92@yahoo.com.ph>
2023-12-02 10:15:45 -0500

    fetched links.json file and displayed it on homepage

--- assets/scripts/links.js
+++ assets/scripts/links.js
@@ -1,3 +1,34 @@
-const baseURL = "https://embrown92.github.io/wdd230/";
+const baseURL = 'https://embrown92.github.io/wdd230/';
+const linksURL = 'https://embrown92.github.io/wdd230/data/links.json';
 
-const linksURL = "";
+const learningActivities = document.querySelector('#learningActivities');
+
+async function getActivityList() {
+    const response = await fetch(linksURL);
+    const activitiesData = await response.json();
+    //console.log(activitiesData);
+    displayWeeks(activitiesData.weeks);
+};
+
+const displayWeeks = (weeks) => {
+    weeks.forEach((week) => {
+        const li = document.createElement('li');
+
+        li.textContent = `${week.week} :`;
+
+        const activityLinksArray = week.links;
+
+        activityLinksArray.forEach((activity) => {
+            const activityURL = document.createElement('a');
+            activityURL.setAttribute('href', `${activity.url}`);
+            activityURL.setAttribute('target', '_blank');
+            activityURL.textContent = `${activity.title} |`;
+            li.appendChild(activityURL);
+        })
+
+        learningActivities.appendChild(li);
+    });
+};
+
+
+getActivityList();
